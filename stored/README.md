609dcc89a6f781bcce8db4623dec72a16b52b71a
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-04-24 12:41:40 +0200

    Update Stored binding
    * Add support for removing values.
    * Uses correct URLs for retrieving values.

--- stored/README.md
+++ stored/README.md
@@ -1,6 +1,6 @@
 # Stored Driver for YCSB
 
-TODO: docs.
+YCSB for Stored.
 
 ## Quickstart
 
@@ -32,19 +32,13 @@ java site.ycsb.CommandLine -db site.ycsb.db.StoredClient -p stored.url=http://12
 Before you can actually run the workload, you need to "load" the data first.
 
 ```
-bin/ycsb load stored -s -P workloads/workloada
+bin/ycsb.sh load stored -s -P workloads/workloada
 ```
 
 Then, you can run the workload:
 
 ```
-bin/ycsb run stored -s -P workloads/workloada
-```
-
-Please see the general instructions in the `doc` folder if you are not sure how it all works. You can apply a property (as seen in the next section) like this:
-
-```
-bin/ycsb run stored -s -P workloads/workloada -p couchbase.useJson=false
+bin/ycsb.sh run stored -s -P workloads/workloada -p stored.mapping_key=/blob/7cc3e1de7953362c40352b7f6f283a040884f5af65cbd07519428ccfdc7e94e39255967fe94b3b23b90fe11f6d84d586d2bd76668287a33d8ab1bfee68fc11b0
 ```
 
 ## Configuration Options
@@ -52,3 +46,4 @@ bin/ycsb run stored -s -P workloads/workloada -p couchbase.useJson=false
 You can set the following properties (with the default settings applied):
 
  - stored.url=http://127.0.0.1:8080 => The URL for one server.
+ - stored.mapping\_key => this is printed when loading a workload.
--- stored/src/main/java/site/ycsb/db/StoredClient.java
+++ stored/src/main/java/site/ycsb/db/StoredClient.java
@@ -10,6 +10,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.ProtocolVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
@@ -35,10 +36,14 @@ import java.util.Vector;
  */
 public class StoredClient extends DB {
   public static final String URL_PROPERTY = "stored.url";
+  public static final String MAPPING_KEY = "stored.mapping_key";
 
   private PoolingHttpClientConnectionManager manager;
   private CloseableHttpClient client;
-  private String url;
+  private String baseUrl;
+  // Maps key provided by the workload to it's path as returned by Stored in the
+  // "Location" header of a response to a POST request.
+  private HashMap<String, String> keyMapping;
 
   @Override
   public void init() throws DBException {
@@ -58,11 +63,25 @@ public class StoredClient extends DB {
       .setUserAgent("YCSB/stored")
       .build();
 
-    this.url = props.getProperty(URL_PROPERTY, "http://127.0.0.1:8080");
+    this.baseUrl = props.getProperty(URL_PROPERTY, "http://127.0.0.1:8080");
+
+    final String mappingKey = props.getProperty(MAPPING_KEY);
+    if (mappingKey == null) {
+      this.keyMapping = new HashMap();
+    } else {
+      try {
+        this.keyMapping = this.readBlob(this.baseUrl + mappingKey);
+      } catch(IOException e) {
+        throw new DBException(e);
+      }
+      if (this.keyMapping == null) {
+        throw new DBException("invalid '" + MAPPING_KEY + "' setting");
+      }
+    }
 
     // Test if the server is running.
     try {
-      HttpGet testRequest = new HttpGet(this.url + "/health");
+      HttpGet testRequest = new HttpGet(this.baseUrl + "/health");
       CloseableHttpResponse testResponse = this.client.execute(testRequest);
 
       try {
@@ -84,6 +103,12 @@ public class StoredClient extends DB {
   @Override
   public void cleanup() throws DBException {
     try {
+      this.insert(null, MAPPING_KEY, StringByteIterator.getByteIteratorMap(this.keyMapping));
+      final String key = this.keyMapping.get(MAPPING_KEY);
+      System.out.println("=====================");
+      System.out.println("Set '" + MAPPING_KEY + "' to '" + key + "' for the run.");
+      System.out.println("=====================");
+
       this.client.close();
       this.manager.shutdown();
     } catch(IOException e) {
@@ -94,8 +119,13 @@ public class StoredClient extends DB {
   @Override
   public Status read(final String table, final String key, final Set<String> fields,
                      final Map<String, ByteIterator> result) {
+    final String url = this.createUrl(key);
+    if (url == null) {
+      return Status.ERROR;
+    }
+
     try {
-      final HttpGet request = new HttpGet(this.url + "/blob/" + key);
+      final HttpGet request = new HttpGet(url);
       final CloseableHttpResponse response = this.client.execute(request);
 
       try {
@@ -103,6 +133,8 @@ public class StoredClient extends DB {
         final int code = status.getStatusCode();
         if (code >= 500) {
           return Status.ERROR;
+        } else if (code == 404 || code == 410) {
+          return Status.NOT_FOUND;
         } else if (code >= 400) {
           return Status.BAD_REQUEST;
         }
@@ -128,6 +160,27 @@ public class StoredClient extends DB {
     }
   }
 
+  private HashMap<String, String> readBlob(final String url) throws IOException {
+    final HttpGet request = new HttpGet(url);
+    final CloseableHttpResponse response = this.client.execute(request);
+
+    try {
+      final StatusLine status = response.getStatusLine();
+      if (status.getStatusCode() != 200) {
+        return null;
+      }
+
+      // Parse the response as a JSON map.
+      final HttpEntity entity = response.getEntity();
+      final ObjectMapper mapper = new ObjectMapper();
+      final HashMap<String, String> values = mapper.readValue(entity.getContent(), HashMap.class);
+
+      return values;
+    } finally {
+      response.close();
+    }
+  }
+
   @Override
   public Status scan(final String table, final String startkey, final int recordcount, final Set<String> fields,
                      final Vector<HashMap<String, ByteIterator>> result) {
@@ -148,7 +201,7 @@ public class StoredClient extends DB {
       final ByteArrayOutputStream body = new ByteArrayOutputStream();
       mapper.writeValue(body, StringByteIterator.getStringMap(values));
 
-      final HttpPost request = new HttpPost(this.url + "/blob");
+      final HttpPost request = new HttpPost(this.baseUrl + "/blob");
       final ByteArrayEntity requestEntity = new ByteArrayEntity(body.toByteArray());
       request.setEntity(requestEntity);
 
@@ -158,7 +211,6 @@ public class StoredClient extends DB {
         final StatusLine status = response.getStatusLine();
         final int code = status.getStatusCode();
         final HttpEntity entity = response.getEntity();
-
 
         if (code >= 500) {
           System.err.print("Server error: '");
@@ -171,6 +223,12 @@ public class StoredClient extends DB {
           System.err.print("'\n");
           return Status.BAD_REQUEST;
         } else if (code == 201) {
+          final String storedUrl = response.getLastHeader("Location").getValue();
+          if (storedUrl == null) {
+            System.err.println("Missing 'Location' header");
+            return Status.ERROR;
+          }
+          this.keyMapping.put(key, storedUrl);
           EntityUtils.consume(entity);
           return Status.OK;
         } else {
@@ -190,7 +248,48 @@ public class StoredClient extends DB {
 
   @Override
   public Status delete(final String table, final String key) {
-    // TODO: implement this in Stored.
-    return Status.NOT_IMPLEMENTED;
+    final String url = this.createUrl(key);
+    if (url == null) {
+      return Status.ERROR;
+    }
+
+    try {
+      final HttpDelete request = new HttpDelete(url);
+      final CloseableHttpResponse response = this.client.execute(request);
+
+      try {
+        final StatusLine status = response.getStatusLine();
+        final int code = status.getStatusCode();
+        if (code >= 500) {
+          return Status.ERROR;
+        } else if (code == 404) {
+          return Status.NOT_FOUND;
+        } else if (code == 410) {
+          return Status.OK;
+        } else {
+          return Status.BAD_REQUEST;
+        }
+      } finally {
+        response.close();
+      }
+    } catch(IOException e) {
+      return Status.ERROR;
+    }
+  }
+
+  /**
+   * Creates the url for the provided key.
+   *
+   * @param key The YCSB provided key.
+   *
+   * @return The full url to get the blob from, or null if no mapping for key
+   * can be found.
+   */
+  private String createUrl(final String key) {
+    final String storedKey = this.keyMapping.get(key);
+    if (storedKey == null) {
+      return null;
+    }
+    return this.baseUrl + storedKey;
   }
 }
